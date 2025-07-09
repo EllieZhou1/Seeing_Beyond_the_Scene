b3a2328e2b69c219ac7926ac6c8ce2d3aebf0920
EllieZhou1 <ellie.m.zhou@gmail.com>
2025-07-09 09:56:37 -0400

    added the stack concat model

--- custom_models.py
+++ custom_models.py
@@ -34,3 +34,44 @@ class SumConcat(nn.Module):
         out = self.layer5(out)
 
         return out
+    
+
+
+#Human Seg Branch ----->
+#                       |--> Stack Concat (after 2nd layer)-->3 more layers --> output
+#Original Branch ------>
+class StackConcat(nn.Module):
+    def __init__ (self, orig_stem, orig_layer1, orig_layer2, seg_stem, seg_layer1,
+                  seg_layer2, layer3, layer4, layer5):
+        super().__init__()
+        self.orig_stem = orig_stem
+        self.orig_layer1 = orig_layer1
+        self.orig_layer2 = orig_layer2
+        self.seg_stem = seg_stem
+        self.seg_layer1 = seg_layer1
+        self.seg_layer2 = seg_layer2
+        self.layer3 = layer3
+        self.layer4 = layer4
+        self.layer5 = layer5
+    
+    def forward (self, orig_img, seg_img):
+        output_orig = self.orig_stem(orig_img)
+        output_orig = self.orig_layer1(output_orig)
+        output_orig = self.orig_layer2(output_orig)
+
+        output_seg = self.seg_stem(seg_img)
+        output_seg = self.seg_layer1(output_seg)
+        output_seg = self.seg_layer2(output_seg)
+
+        # print("output_orig shape before concat", output_orig)
+        # print("output_orig shape before concat", output_seg)
+
+        concat = torch.cat([output_orig, output_seg], dim=1)
+
+        #print("shape after concat: ", concat.shape)
+
+        out = self.layer3(concat)
+        out = self.layer4(out)
+        out = self.layer5(out)
+
+        return out
