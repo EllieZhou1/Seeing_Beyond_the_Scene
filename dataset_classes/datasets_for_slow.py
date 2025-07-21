6b392b6c6a5a9644c87d34a0a0263fc523cf38dd
EllieZhou1 <ellie.m.zhou@gmail.com>
2025-07-21 09:57:16 -0400

    - added isHATActionSwap as a parameter to be passed in - Made changes to make it work with all datasets

--- dataset_classes/datasets_for_slow.py
+++ dataset_classes/datasets_for_slow.py
@@ -63,10 +63,11 @@ kinetics_classname_to_id = {v: k for k, v in kinetics_id_to_classname.items()}
 # ========== DATA PIPELINE ==========
 
 class DatasetSlow(torch.utils.data.Dataset):
-    def __init__(self, csv_path, col, max_videos=None):
+    def __init__(self, csv_path, col, isHATActionSwap, max_videos=None):
         self.max_videos = max_videos
         self.col = col
         self.df = pd.read_csv(csv_path)
+        self.isHATActionSwap = isHATActionSwap
 
         #reduce to max videos if specified
         # self.df = self.df.sample(n=max_videos) if max_videos is not None else self.df
@@ -81,11 +82,8 @@ class DatasetSlow(torch.utils.data.Dataset):
 
     
     # Compute indices for 8 and 32 evenly spaced frames
-    def sample_indices_oneidx(self, n, total_frames):
+    def sample_indices(self, n, total_frames):
         return [int(round(i * (total_frames - 1) / (n - 1) + 1)) for i in range(n)]
-    
-    def sample_indices_zeroidx(self, n, total_frames):
-        return [int(round(i * (total_frames - 1) / (n - 1))) for i in range(n)]
     
     #Given the path to the frames directory and a list of indicies, load the video frames
     #Returns a tensor of the video frames
@@ -104,26 +102,30 @@ class DatasetSlow(torch.utils.data.Dataset):
 
     def __getitem__(self, idx): #only outputs one tensor as opposed to
         row = self.df.iloc[idx]
-        label = row['label']
-        full_path = row['full_path']
 
-        total_frames = row ['num_files']
+        if self.isHATActionSwap:
+            label = row['label_A']
+            backgroundLabel = row['label_B']
+            total_frames = row['num_files_A']
+        else:
+            label = row['label']
+            backgroundLabel = None
+            total_frames = row['num_files']
 
-        #comes from places365 --> 0-31
-
-        if self.col == 'full_path':
+        if self.col == 'full_path' or self.col == 'action_swap_path':
             if full_path.startswith("/n/fs/visualai-scr/temp_LLP/ellie/slowfast_kinetics/dataset/places365/"):
-                idx_slow = self.sample_indices_zeroidx(num_frames_slow, 32)
+                idx_slow = self.sample_indices(num_frames_slow, 32)
             else:
-                idx_slow = self.sample_indices_oneidx(num_frames_slow, total_frames)
+                idx_slow = self.sample_indices(num_frames_slow, total_frames)
         elif self.col == 'segmented_path':
-            idx_slow = self.sample_indices_zeroidx(num_frames_slow, 32)
+            idx_slow = self.sample_indices(num_frames_slow, 32)
 
         slow_tensor = self.load_video_frames(row[self.col], idx_slow)
 
         result = {
             "inputs": slow_tensor,
-            "label": kinetics_classname_to_id[label]
+            "label": kinetics_classname_to_id[label],
+            "label_background":backgroundLabel
         }
         return result
     
